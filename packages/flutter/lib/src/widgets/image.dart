084fa179624531c8816ed8bf6cfe5c9352cb7186
sajjadalijaved <sajjadali11755@gmail.com>
2023-02-23 21:33:11 +0500

    start

--- packages/flutter/lib/src/widgets/image.dart
+++ packages/flutter/lib/src/widgets/image.dart
@@ -1141,7 +1141,7 @@ class _ImageState extends State<Image> with WidgetsBindingObserver {
                 assert(() {
                   if (widget.errorBuilder == null) {
                     // ignore: only_throw_errors, since we're just proxying the error.
-                    throw error; // Ensures the error message is printed to the console.
+                  throw error; // Ensures the error message is printed to the console.
                   }
                   return true;
                 }());
